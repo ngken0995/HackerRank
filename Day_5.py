3dea42a25a77777da2db46c8be08d2a3ea35561b
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2020-01-13 11:34:38 -0500

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -1,20 +1,16 @@
-class Person:
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on     initialAge
-        if initialAge < 0:
-            print('Age is not valid, setting age to 0.')
-            self.age= 0
-        else:
-            self.age = initialAge
-    def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if self.age < 13:
-            print('You are young.')
-        elif self.age < 18:
-            print('You are a teenager.')
-        else:
-            print('You are old.')
+#!/bin/python3
 
-    def yearPasses(self):
-        # Increment the age of the person in here
-        self.age += 1
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for num in range(1, 11):
+        z=n*num
+        print(f'{n} x {num} = {z}')
+    
