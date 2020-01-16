eabf23e6dee043897f482eba94da73b2c9282ba5
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2020-01-16 10:44:20 -0500

    Update Day_5.py

--- Day_5.py
+++ Day_5.py
@@ -13,4 +13,12 @@ if __name__ == '__main__':
     for num in range(1, 11):
         z=n*num
         print(f'{n} x {num} = {z}')
-    
+
+#Practicing how to use "def"       
+#def answer(n):
+#  for num in range(1, 11):
+#    z=n*num
+#    print(f'{n} x {num} = {z}')    
+
+#n = int(input()
+#answer(n)
