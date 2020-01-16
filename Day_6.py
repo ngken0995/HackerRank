4eb9104540971b6147fa056c4323b04db06d99a5
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2020-01-16 14:00:22 -0500

    Update Day_6.py

--- Day_6.py
+++ Day_6.py
@@ -17,3 +17,29 @@ for j in range(0,len(word)):
   a =''.join(a)
   b =''.join(b)
   print(f'{a} {b}')
+
+#Using function
+word=[]
+
+def wordgroup():
+  global word
+  t = int(input())
+  for i in range(0, t):
+    s = input()
+    word.append(s)
+  return word
+
+def separate(word):
+  for j in range(0,len(word)):
+    a=[]
+    b=[]
+    for k in range(0,len(word[j])):
+      if k == 0:
+        a.append(word[j][k])
+      elif k%2 == 1:
+        b.append(word[j][k])
+      elif k%2 == 0:
+        a.append(word[j][k])
+    a =''.join(a)
+    b =''.join(b)
+    print(f'{a} {b}')
