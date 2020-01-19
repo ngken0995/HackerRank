5edab0b5350a6517435af70b892a1e52496b056b
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2020-01-19 17:05:59 -0500

    Update Day_11.py

--- Day_11.py
+++ Day_11.py
@@ -14,12 +14,13 @@ if __name__ == '__main__':
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    value =[]
+    value = 0
     for i in range(0,4):
         for j in range(0,4):
             hourglass = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
-            value.append(hourglass)
+            if hourglass > value:
+              value = hourglass
 
-    print(max(value))        
+    print(value)        
 
 
